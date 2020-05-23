fcee150fff1049490e4c346f0bc748a07079e797
Charlie9830 <charliewjhall@gmail.com>
2020-05-23 14:43:32 +1000

    Reset Home Widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:darkwrong/containers/HomeScreenContainer.dart';
 import 'package:darkwrong/presentation/DarkwrongScaffold/DarkwrongScaffold.dart';
 import 'package:darkwrong/presentation/tool_rail/ToolRail.dart';
 import 'package:darkwrong/presentation/tool_rail/ToolRailOption.dart';
@@ -29,39 +30,7 @@ class _DarkwrongState extends State<Darkwrong> {
             brightness: Brightness.dark,
             visualDensity: VisualDensity.adaptivePlatformDensity,
           ),
-          home: DarkwrongScaffold(
-            appBar: AppBar(title: Text('Widget')),
-            body: Container(
-              color: Colors.lightGreen,
-              child: ListView(
-                children: List<Widget>.generate(101, (index) => ListTile(title: Text('Item $index'))),
-              )
-            ),
-            persistentLeftRail: true,
-            leftRail: ToolRail(
-              selectedValue: _selectedValue,
-              options: <ToolRailOption>[
-                ToolRailOption(
-                  icon: Icon(Icons.add_circle),
-                  value: 'add',
-                  selected: _selectedValue == 'add',
-                  onSelected: (value) => setState(() => _selectedValue =
-                      value == _selectedValue ? null : value),
-                ),
-                ToolRailOption(
-                  icon: Icon(Icons.filter_list),
-                  value: 'view',
-                  selected: _selectedValue == 'view',
-                  onSelected: (value) => setState(() => _selectedValue =
-                      value == _selectedValue ? null : value),
-                ),
-              ],
-              children: <Widget>[
-                Text('Add Fixtures'),
-                Text('View Fixtures'),
-              ],
-            ),
-          ),
+          home: HomeScreenContainer(),
         ));
   }
 }
